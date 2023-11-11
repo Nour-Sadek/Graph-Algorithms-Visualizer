3047ab47faf4c16c8a646156093e4a42eecc0dcd
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-11-11 18:14:18 -0500

    Implemented MouseListener as an anonymous class to graphPanel

--- visualizer/MainFrame.java
+++ visualizer/MainFrame.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class MainFrame extends JFrame implements MouseListener {
+public class MainFrame extends JFrame {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
     protected static final Color BACKGROUND_COLOR = Color.black;
@@ -14,59 +14,55 @@ public class MainFrame extends JFrame implements MouseListener {
         super("Graph-Algorithms Visualizer");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        addMouseListener(this);
         setSize(MainFrame.WIDTH, MainFrame.HEIGHT);
 
         setJPanel();
-
     }
 
     private void setJPanel() {
         this.graphPanel = new JPanel();
         graphPanel.setName("Graph");
         graphPanel.setBackground(MainFrame.BACKGROUND_COLOR);
-        //graphPanel.setBounds(0, 0, MainFrame.WIDTH, MainFrame.HEIGHT);
         graphPanel.setLayout(null);
         this.add(graphPanel);
-
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // Cancel gives null
-        // OK gives input entered by user (empty string if user doesn't give input)
-        while (true) {
-            String input = JOptionPane.showInputDialog(this, "Enter the Vertex ID (Should be 1 char):",
-                    "Vertex", JOptionPane.QUESTION_MESSAGE);
-            if (input == null) {
-                break;
-            } else {
-                input = input.trim();
-                if (input.length() == 1) {
-                    int xValue = e.getX();
-                    int yValue = e.getY();
-                    this.createVertex(xValue, yValue, input);
-                    return;
+        graphPanel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                while (true) {
+                    String input = JOptionPane.showInputDialog(graphPanel, "Enter the Vertex ID (Should be 1 char):",
+                            "Vertex", JOptionPane.QUESTION_MESSAGE);
+                    if (input == null) {
+                        break;
+                    } else {
+                        input = input.trim();
+                        if (input.length() == 1) {
+                            int xValue = e.getX();
+                            int yValue = e.getY();
+                            createVertex(graphPanel, xValue, yValue, input);
+                            return;
+                        }
+                    }
                 }
             }
-        }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+
+            private static void createVertex(JPanel graphPanel, int xValue, int yValue, String id) {
+                JPanel vertex = new Vertex(xValue, yValue, id);
+                graphPanel.add(vertex);
+                vertex.repaint();
+            }
+        });
+
     }
-
-    private void createVertex(int xValue, int yValue, String id) {
-        JPanel vertex = new Vertex(xValue, yValue, id);
-        this.graphPanel.add(vertex);
-        vertex.repaint();
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {}
-
-    @Override
-    public void mouseReleased(MouseEvent e) {}
-
-    @Override
-    public void mouseEntered(MouseEvent e) {}
-
-    @Override
-    public void mouseExited(MouseEvent e) {}
 }
