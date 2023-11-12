e5599fa1482658492e30c91c8f6957a7ad5d1a13
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-11-12 13:41:11 -0500

    Added a menu for different modes and a label to display the current mode

--- visualizer/MainFrame.java
+++ visualizer/MainFrame.java
@@ -10,21 +10,28 @@ public class MainFrame extends JFrame {
     private static final int HEIGHT = 600;
     protected static final Color BACKGROUND_COLOR = Color.black;
     private JPanel graphPanel;
+    private JLabel modeLabel;
     public MainFrame() {
         super("Graph-Algorithms Visualizer");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
         setSize(MainFrame.WIDTH, MainFrame.HEIGHT);
+        setLayout(null);
+        setLocationRelativeTo(null);
 
+        setJMenu();
         setJPanel();
+        setModeJLabel();
+
+        setVisible(true);
+
     }
 
     private void setJPanel() {
-        this.graphPanel = new JPanel();
+        this.graphPanel = (JPanel) this.getContentPane();
         graphPanel.setName("Graph");
         graphPanel.setBackground(MainFrame.BACKGROUND_COLOR);
         graphPanel.setLayout(null);
-        this.add(graphPanel);
+        graphPanel.setSize(MainFrame.WIDTH, MainFrame.HEIGHT);
         graphPanel.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -36,8 +43,8 @@ public class MainFrame extends JFrame {
                     } else {
                         input = input.trim();
                         if (input.length() == 1) {
-                            int xValue = e.getX();
-                            int yValue = e.getY();
+                            int xValue = e.getX() - Vertex.WIDTH / 2;
+                            int yValue = e.getY() - Vertex.HEIGHT / 2;
                             createVertex(graphPanel, xValue, yValue, input);
                             return;
                         }
@@ -63,6 +70,59 @@ public class MainFrame extends JFrame {
                 vertex.repaint();
             }
         });
+
+    }
+
+    private void setJMenu() {
+        // Creating the menuBar
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+
+        // Creating the menu "Mode"
+        JMenu modeMenu = new JMenu("Mode");
+        menuBar.add(modeMenu);
+
+        // Creating the three menu items for "Mode"
+        JMenuItem addAVertex = new JMenuItem("Add a Vertex");
+        JMenuItem addAnEdge = new JMenuItem("Add an Edge");
+        JMenuItem none = new JMenuItem("None");
+
+        modeMenu.add(addAVertex);
+        modeMenu.add(addAnEdge);
+        modeMenu.add(none);
+
+        // Add event listeners to the three menu items
+        addAVertex.addActionListener(e -> {
+            // Change text for label
+            changeTextForModeLabel("Current Mode -> Add a Vertex");
+        });  // TODO
+        addAnEdge.addActionListener(e -> {
+            // Change text for label
+            changeTextForModeLabel("Current Mode -> Add an Edge");
+        });  // TODO
+        none.addActionListener(e -> {
+            // Change text for label
+            changeTextForModeLabel("Current Mode -> None");
+        });  // TODO
+    }
+
+    private void changeTextForModeLabel(String newText) {
+        this.modeLabel.setText(newText);
+        modeLabel.setSize(modeLabel.getPreferredSize());
+        this.modeLabel.setLocation(MainFrame.WIDTH - modeLabel.getWidth() - 20, 0);
+    }
+
+    private void setModeJLabel() {
+
+        modeLabel = new JLabel();
+        this.graphPanel.add(modeLabel);
+        modeLabel.setName("Mode");
+        modeLabel.setText("Current Mode -> Add a Vertex");
+        modeLabel.setOpaque(true);
+        modeLabel.setForeground(Vertex.VERTEX_COLOR);
+        modeLabel.setBackground(MainFrame.BACKGROUND_COLOR);
+        modeLabel.setSize(modeLabel.getPreferredSize());
+        modeLabel.setLocation(MainFrame.WIDTH - modeLabel.getWidth() - 20, 0);
 
     }
 }
