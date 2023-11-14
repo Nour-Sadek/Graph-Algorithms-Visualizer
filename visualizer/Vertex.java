3b4c2b693b478e1bf540be6509bea0798a76cb02
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-11-14 18:09:34 -0500

    Added a new instance-attribute edges

--- visualizer/Vertex.java
+++ visualizer/Vertex.java
@@ -3,34 +3,18 @@ package visualizer;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class Vertex extends JPanel {
 
-    protected static final int WIDTH = 50;
-    protected static final int HEIGHT = 50;
+    protected static final int SIZE = 50;
     protected static final Color VERTEX_COLOR = Color.white;
     protected static final Map<String, Vertex> vertices = new HashMap<>();
     private String id;
     private JLabel label;
     private int xLocation;
     private int yLocation;
-
-    /*
-    public Vertex(int x, int y) {
-
-        this.id = String.valueOf(vertices.size());
-        vertices.put(this.id, this);
-        this.label = new JLabel("VertexLabel " + this.id);
-        this.setLabel();
-
-        this.setName("Vertex " + this.id);
-        this.setBackground(MainFrame.BACKGROUND_COLOR);
-        this.setLayout(null);
-        this.setBounds(x, y, Vertex.WIDTH, Vertex.HEIGHT);
-
-    }
-
-     */
+    private List<Edge> edges = new ArrayList<>();
 
     public Vertex(int x, int y, String id) {
         this.id = id;
@@ -45,7 +29,7 @@ public class Vertex extends JPanel {
         this.setBackground(MainFrame.BACKGROUND_COLOR);
         this.setOpaque(false);
         this.setLayout(null);
-        this.setBounds(x, y, Vertex.WIDTH, Vertex.HEIGHT);
+        this.setBounds(x, y, Vertex.SIZE, Vertex.SIZE);
     }
 
     public int getXLocation() {
@@ -59,19 +43,20 @@ public class Vertex extends JPanel {
     public String getId() {
         return this.id;
     }
+    public List<Edge> getEdges() { return this.edges; }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Vertex.VERTEX_COLOR);
-        g.fillOval(0, 0, Vertex.WIDTH, Vertex.HEIGHT);
+        g.fillOval(0, 0, Vertex.SIZE, Vertex.SIZE);
     }
 
     private void setLabel() {
-        this.label.setText(String.valueOf(this.id));
+        this.label.setText(this.id);
         this.label.setVerticalAlignment(JLabel.CENTER);
         this.label.setHorizontalAlignment(JLabel.CENTER);
-        this.label.setSize(Vertex.WIDTH, Vertex.HEIGHT);
+        this.label.setSize(Vertex.SIZE, Vertex.SIZE);
         this.add(this.label);
     }
 
