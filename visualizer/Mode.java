1ba5eed7630a8f63e69c89526c4834c558c6cdc5
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-11-15 19:00:08 -0500

    Added two new Modes: Remove an Edge and Remove a Vertex

--- visualizer/Mode.java
+++ visualizer/Mode.java
@@ -2,7 +2,11 @@ package visualizer;
 
 public enum Mode {
 
-    NONE("None"), ADD_A_VERTEX("Add a Vertex"), ADD_AN_EDGE("Add an Edge");
+    NONE("None"),
+    ADD_A_VERTEX("Add a Vertex"),
+    ADD_AN_EDGE("Add an Edge"),
+    REMOVE_A_VERTEX("Remove a Vertex"),
+    REMOVE_AN_EDGE("Remove an Edge");
     private final String description;
 
     Mode(String description) {
